88619cb61ea271bd23d50ae4a6d0782069b9ea29
Vincent Li <v.li@f5.com>
2021-06-20 09:28:58 -0700

    make sure self ip subnet mask to be /16, since cilium pod ip is 10.0.x.x, /24 may lead to pod ARP unresolved error
    for example:
    
    Invalid neighbor entry (10.0.1.146), the IP address must be in a directly connected subnet

--- cilium-bigip/INSTALL.txt.sh
+++ cilium-bigip/INSTALL.txt.sh
@@ -10,10 +10,14 @@ tmsh create net tunnels vxlan fl-vxlan port 8472 flooding-type none
 tmsh create net tunnels tunnel flannel_vxlan key 68 profile fl-vxlan local-address 10.169.72.34 
 
 # 3. Create the VXLAN tunnel self-IP, allow default service, allow none stops self ip ping.
-tmsh create net self 10.0.3.34 address 10.0.3.34/255.255.255.0 allow-service default vlan flannel_vxlan
+#    also make sure selfip subnet is /16, if /24, it may lead to unresolved pod ARP
+tmsh create net self 10.0.3.34 address 10.0.3.34/255.255.0.0 allow-service default vlan flannel_vxlan
 
 # 4. Create partition "k8s" on BIG-IP
 tmsh create auth partition k8s
+
+# 5. save config on BIG-IP
+tmsh save sys config
 
 Cilium:
 
